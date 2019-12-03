10cfb7411f0c398601261eac459c4e87606124fa
Kelvin Brito <britok30@gmail.com>
2019-12-03 00:20:56 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-fluid">
+        <Header />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+  background: #181818;
+  color: #fff;
+}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="/">
+        <ion-icon name="logo-apple" class="logo"></ion-icon>Music.vue
+      </a>
+    </nav>
+    <h1>Welcome</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+.navbar-brand {
+  font-size: 4.3rem;
+  font-weight: 200;
+}
+
+.logo {
+  font-size: 4.3rem;
+}
+
+#header {
+  height: 75vh;
+  background: linear-gradient(rgba(0, 0, 0, 0.311), rgba(0, 0, 0, 0.311)),
+    url("../assets/hero.jpg") no-repeat center center/cover;
+}
+
+h1 {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-weight: 200;
+  font-size: 4rem;
+  letter-spacing: 5px;
+}
+</style>
+
