6d360ca3c5e7fe286b787f0050003546a7ac838f
Kelvin Brito <britok30@gmail.com>
2019-12-06 00:59:21 -0500

    weather time

---
+++ src/components/Weather/WeatherShow.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="showcase" v-scroll-reveal.reset="animate1">
+    <h2 class="lead-head">Weather</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherShow",
+  data(){
+    return {
+      animate1: {
+        delay: 350,
+        duration: 3500,
+        origin: "bottom",
+        distance: "30px"
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.showcase {
+  background: linear-gradient(rgba(0, 0, 0, 0.40), rgba(0, 0, 0, 0.40)),
+    url("../../assets/weather.jpg") no-repeat center center/cover;
+  height: 60vh;
+  position: relative;
+  border-radius: 5px;
+}
+
+.lead-head {
+  font-size: 5rem;
+  font-weight: 700;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
