b3ee1142375d9b3e97f67feb95402ae7c99c3812
Kelvin Brito <britok30@gmail.com>
2019-12-06 00:15:16 -0500

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>DayViewTimes &copy;</p>
+    <p>
+      Powered by
+      <a href="https://newsapi.org/">News Api</a>
+    </p>
+    <p>
+      Powered by
+      <a href="https://www.worldtradingdata.com/">World Trading Data</a>
+    </p>
+    <p>Code & Design by Brito</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
