8b96286e18c7dbd75924ea30f1d4d51520a8d83d
Kelvin Brito <britok30@gmail.com>
2019-12-07 23:56:55 -0500

    not found page

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <h1>
-      404. Page Not Found. The page you're trying to access does not exist.
-    </h1>
+  <div class="notfound">
+    <div class="notfoundtext">
+      <h1>
+        404. Page Not Found. The page you're trying to access does not exist.
+      </h1>
+      <router-link to="/" class="btn btn-outline-light"
+        >Return to Home</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -12,4 +17,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.notfound {
+  background: linear-gradient(rgba(0, 0, 0, 0.452), rgba(0, 0, 0, 0.452)),
+    url("../assets/notfound1.jpg") no-repeat center center/cover;
+  height: 100vh;
+}
+.btn {
+  font-size: 1.3rem;
+  margin-top: 3rem;
+}
+
+.notfoundtext {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
